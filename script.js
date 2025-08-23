2da5c11a5ab6ccb80835e86f69bd38a8eb363060
Rooney-tech <odagorooney6@gmail.com>
2025-08-23 09:44:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["BI Analyst","M&E Assistant","Data Scientist","Statistician"],
+        strings: ["Statistician","Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
