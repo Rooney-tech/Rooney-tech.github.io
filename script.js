20c3fab7dcd99f1ac713296fd6c776881a216d9f
Rooney-tech <odagorooney6@gmail.com>
2025-08-23 10:58:13 +0300

    Update Carousel

--- script.js
+++ script.js
@@ -53,13 +53,14 @@ $(document).ready(function(){
         margin: 15,
         loop: true,
         autoplay: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         responsive: {
             0:{
                 items: 1,
                 nav: false
-            } ,
+            } 
+            ,
             400:{
                 items: 2,
                 nav: false
